1c244b9d566e2628888c423bf52ad653c881a433
vibuda <vsiromin@gmail.com>
2023-02-14 23:13:08 +0530

    Started Settings Secion

---
+++ miracle-workers/src/components/setting section/Settings.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SettingsItem from './SettingsItem';
+
+const Settings = () => {
+  return (
+    <div>
+        <SettingsItem title="Command"/>
+        <SettingsItem title="Browser"/>
+        <SettingsItem title="Test Type"/>
+
+    </div>
+  )
+}
+
+export default Settings
---
+++ miracle-workers/src/components/setting section/SettingsItem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './SettingsItem.css';
+
+const SettingsItem = ({title}) => {
+  return (
+    <div className='setting'>{title}</div>
+  )
+}
+
+export default SettingsItem
