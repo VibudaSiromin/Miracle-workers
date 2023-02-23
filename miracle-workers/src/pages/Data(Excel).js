7b6486aeee30dd57d3ffe4345f7dd6837a5d0bb1
Isuru <isurumadushangamage99@gmail.com>
2023-02-24 01:09:28 +0530

    completed functionalities of fileUploader

--- miracle-workers/src/pages/Data(Excel).js
+++ miracle-workers/src/pages/Data(Excel).js
@@ -16,11 +16,45 @@ const ExcelSection = (props) => {
     setFileData(excelData);
   }
 
+  //same file data is loaded when reload button is clicked
+  const reloadFileDataHandler = (data) => {
+    const reloadExcelData=[...data];
+    setFileData(reloadExcelData);
+  }
+  //same file headings are loaded when reload button is clicked
+  const reloadFileHeadersHandler = (Headers) => {
+    const reloadExcelHeaders=[...Headers];
+    setFileHeaders(reloadExcelHeaders);
+  }
+  
+  const fileDataDeleteHandler = () => {
+    console.log('Cinamon');
+    fileData.splice(0,fileData.length);
+    const emptyFileData=[...fileData]
+    setFileData(emptyFileData);
+  }
+
+  const fileHeaderdeleteHandler = () => {
+    console.log('Garlic');
+    fileHeaders.splice(0,fileHeaders.length)
+    const emptyFileHeaders=[...fileHeaders]
+    console.log()
+    setFileHeaders(emptyFileHeaders)
+  }
+
   console.log('MSI mchn');
 
 return(
         <>
-          <FileUploader getFileHeaders={fileHeadersHandler} getFileData={fileDataHandler}></FileUploader>
+          <FileUploader 
+            getFileHeaders={fileHeadersHandler} 
+            getFileData={fileDataHandler} 
+            reloadFileData={reloadFileDataHandler} 
+            reloadFileHeaders={reloadFileHeadersHandler} 
+            deleteFileData={fileDataDeleteHandler}
+            deleteFileHeaders={fileHeaderdeleteHandler}
+            >
+          </FileUploader>
           <Heading noFields={[1]} generalPurpose={true}  addHeading="addHeading" removeHeading={true} initialHeading={fileHeaders} initialData={fileData}></Heading>
         </>
         
