8c9980528fe41619c8adc1d671b617ffdd8fc82a
vibuda <vsiromin@gmail.com>
2023-06-04 11:28:20 +0530

    updated

--- miracle-workers/src/Components/PopUpInputField.js
+++ miracle-workers/src/Components/PopUpInputField.js
@@ -1,17 +1,37 @@
 import { useState } from "react";
 import Mapper from "./Mapper";
+import * as yup from "yup";
 
 const PopUpInputField = (props) => {
 
+    let validation;
+    let error;
+
+    switch(props.title){
+      case "group":
+        validation=yup.string().required().min(3).max(10);
+        break;
+    }
+
+
+
     const [referenceValue,setReferenceValue]=useState('');
 
-    const inputHandler=(event) => {
+    const inputHandler=async (event) => {
       const fieldName = event.target.getAttribute("name");
       const fieldValue = event.target.value;
       console.log(fieldName);
       console.log(fieldValue);
       console.log('Input id: '+ props.id);
       console.log('generalPurpose: '+ props.generalPurpose);
+
+      try{
+        const isValid=await validation.validate(fieldValue);
+        console.log("valiiiiiiiiiii",isValid);
+        // error=null;
+      }catch(err){
+        error=err
+      }
 
       if(props.id<=2 && props.generalPurpose===false){
         props.onDataChange(fieldName,fieldValue);
@@ -31,6 +51,7 @@ const PopUpInputField = (props) => {
   }
 
     console.log("Moon",props.title);
+
     if(props.title==='data'){
       return(
         <div className="form-group">
@@ -44,7 +65,7 @@ const PopUpInputField = (props) => {
         <div className="form-group">
             <label>{props.title}</label>
             <input type={props.inputType} className="form-control" name={props.title}  onChange={inputHandler}></input>
-            <small className="text-danger">{errors[props.title]?.message}</small>
+            {error?<small className="text-danger">{error}</small>:null}
         </div>
     );
     }
