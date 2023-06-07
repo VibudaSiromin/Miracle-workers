21c83b3d345a4c99b6e04d1c86f65b58a546c5fe
PramodaS <prmdpriyadarshana@gmail.com>
2023-06-07 21:07:43 +0530

    changes

--- miracle-workers-backend/controllers/json-upload/json-upload-controller.js
+++ miracle-workers-backend/controllers/json-upload/json-upload-controller.js
@@ -1,10 +1,16 @@
 const Json = require("../../models/json-models/json");
+const path = require("path");
+const fs = require("fs");
+const launcherFilePath = path.join(__dirname, "../../store/launcher.json");
+const testSuiteFilePath = path.join(__dirname, "../../store/testSuite.json");
 
 const uploadJson = async (req, res, next) => {
   try {
-    const { file } = req.body;
-    const save = new Json({ file: file });
-    return res.status(200).json({ message: "File added", data: save });
+    const { launcher, testSuite } = req.body;
+    console.log(1111111);
+    await fs.promises.writeFile(launcherFilePath, JSON.stringify(launcher));
+    await fs.promises.writeFile(testSuiteFilePath, JSON.stringify(testSuite));
+    return res.status(200).json({ message: "File added" });
   } catch (error) {
     console.log(error);
   }
--- miracle-workers-backend/controllers/locator-controllers/locator.js
+++ miracle-workers-backend/controllers/locator-controllers/locator.js
@@ -1,130 +1,133 @@
-const fs = require('fs');
-const path = require('path');
-const { argv0 } = require('process');
+const fs = require("fs");
+const path = require("path");
+const { argv0 } = require("process");
 
 // const dataFilePath =path.join(__dirname,'locatorFile.json');
 
-const dataFilePath=path.join(__dirname,'../../store/locator.json');
+const dataFilePath = path.join(__dirname, "../../store/locator.json");
 
-
-//get locator sheet names 
-const getPageNames=async(req, res, next)=>{
-  const locatorName=req.body.lname;
+//get locator sheet names
+const getPageNames = async (req, res, next) => {
+  const locatorName = req.body.lname;
   let locatorFile;
-  try{
+  try {
     const data = await fs.promises.readFile(dataFilePath);
     locatorFile = JSON.parse(data);
-  }catch{
-    console.log(err);
-    res.status(500).json({ message: 'Error reading locator file' });
+    console.log(locatorFile);
+  } catch {
+    // console.log(err);
+    res.status(500).json({ message: "Error reading locator file" });
     return;
   }
-  const pageNamesArray=locatorFile.map(locator=>locator[0]);
-  console.log("Doooo",pageNamesArray)
+  const pageNamesArray = locatorFile.map((locator) => locator[0]);
+  console.log("Doooo", pageNamesArray);
   res.json({ locatorsPageNames: pageNamesArray });
-}
+};
 
-const getLocatorByPage = async(req, res, next) => {
+const getLocatorByPage = async (req, res, next) => {
   let locatorFile;
-  const locatorPageName=req.params.lname;
-  try{
-      const data = await fs.promises.readFile(dataFilePath);
-      locatorFile = JSON.parse(data);
-    }catch{ 
-      console.log(err);
-      res.status(500).json({ message: 'Error reading locator file' });
-      return;
-    }
-    locatorPage=locatorFile.find(locator=>locator[0]===locatorPageName);
-    arrayWithoutLocatorName=locatorPage.slice(1,locatorPage.length)
-    res.json({ locators: arrayWithoutLocatorName});
-  };   
+  const locatorPageName = req.params.lname;
+  try {
+    const data = await fs.promises.readFile(dataFilePath);
+    locatorFile = JSON.parse(data);
+  } catch {
+    // console.log(err);
+    res.status(500).json({ message: "Error reading locator file" });
+    return;
+  }
+  locatorPage = locatorFile.find((locator) => locator[0] === locatorPageName);
+  arrayWithoutLocatorName = locatorPage.slice(1, locatorPage.length);
+  res.json({ locators: arrayWithoutLocatorName });
+};
 
+const createLocatorByPage = async (req, res, next) => {
+  let locatorFile;
+  const locatorPageName = req.body.pageName;
+  try {
+    const data = await fs.promises.readFile(dataFilePath);
+    locatorFile = JSON.parse(data);
+  } catch {
+    // console.log(err);
+    res.status(500).json({ message: "Error reading locator file" });
+    return;
+  }
+  locatorFile.push([locatorPageName]);
+  const newData = JSON.stringify(locatorFile);
+  try {
+    await fs.promises.writeFile(dataFilePath, newData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Error occurred when creating locator" });
+    return;
+  }
+  res.status(200).json({ message: "Created locator Page" });
+};
 
-  const createLocatorByPage = async (req, res, next) => {
-    let locatorFile;
-    const locatorPageName=req.body.pageName;
-    try{
-        const data = await fs.promises.readFile(dataFilePath);
-        locatorFile = JSON.parse(data);
-      }catch{
-        console.log(err);
-        res.status(500).json({ message: 'Error reading locator file' });
-        return;
-      }
-    locatorFile.push([locatorPageName]);
-    const newData=JSON.stringify(locatorFile);
-    try{
-      await fs.promises.writeFile(dataFilePath,newData);
-    }catch(err){
-      console.log(err);
-      res.status(500).json({message:'Error occurred when creating locator'});
-      return;
-    }
-    res.status(200).json({message:'Created locator Page'});
-  };
+const editLocatorPage = async (req, res, next) => {
+  const locatorPageName = req.params.lname;
+  const newLocator = req.body.editedLocator;
+  console.log("gooooo", newLocator);
 
-const editLocatorPage=async (req,res,next)=>{
-    const locatorPageName=req.params.lname;
-    const newLocator=req.body.editedLocator;
-    console.log("gooooo",newLocator)
-    
-    let locatorFile;
-    try{
-      const data = await fs.promises.readFile(dataFilePath);
-      locatorFile = JSON.parse(data);   
-    }catch(err){
-      console.log(err);
-      res.status(500).json({ message: 'Error reading locator file' });
-      return;
-    }
-    const index = locatorFile.findIndex(locator=>locator[0]===locatorPageName);
-    const newLocatorPage=[];
-    newLocatorPage[0]=locatorPageName;
-    for(let i=0;i<newLocator.length;i++){
-      newLocatorPage[i+1]=newLocator[i]
-    }
-    console.log("Hoooo",newLocatorPage);
-    locatorFile[index]=newLocatorPage;
-    newData=JSON.stringify(locatorFile);
-    console.log("Yoooo",newData)
-    try{
-      await fs.promises.writeFile(dataFilePath,newData);
-    }catch(err){
-      console.log(err);
-      res.status(500).json({message:'Error occurred when creating locator'});
-      return;
-    }
-    res.status(200).json({message:'Edited locator'});
-}
+  let locatorFile;
+  try {
+    const data = await fs.promises.readFile(dataFilePath);
+    locatorFile = JSON.parse(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Error reading locator file" });
+    return;
+  }
+  const index = locatorFile.findIndex(
+    (locator) => locator[0] === locatorPageName
+  );
+  const newLocatorPage = [];
+  newLocatorPage[0] = locatorPageName;
+  for (let i = 0; i < newLocator.length; i++) {
+    newLocatorPage[i + 1] = newLocator[i];
+  }
+  console.log("Hoooo", newLocatorPage);
+  locatorFile[index] = newLocatorPage;
+  newData = JSON.stringify(locatorFile);
+  console.log("Yoooo", newData);
+  try {
+    await fs.promises.writeFile(dataFilePath, newData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Error occurred when creating locator" });
+    return;
+  }
+  res.status(200).json({ message: "Edited locator" });
+};
 
-const deleteLocatorPage=async(req,res,next)=>{
-    const locatorPageName=req.params.lname;
-    let locatorFile;
-    try{
-      data = await fs.promises.readFile(dataFilePath);
-      locatorFile = JSON.parse(data);
-    }catch(err){
-      console.log(err);
-      res.status(500).json({ message: 'Error reading locator file' });
-      return;
-    }
-    const index = locatorFile.findIndex(locator=>locator[0]===locatorPageName);
-    locatorFile.splice(index,1);
+const deleteLocatorPage = async (req, res, next) => {
+  const locatorPageName = req.params.lname;
+  let locatorFile;
+  try {
+    data = await fs.promises.readFile(dataFilePath);
+    locatorFile = JSON.parse(data);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Error reading locator file" });
+    return;
+  }
+  const index = locatorFile.findIndex(
+    (locator) => locator[0] === locatorPageName
+  );
+  locatorFile.splice(index, 1);
 
-    const newData=JSON.stringify(locatorFile);
-    try{
-      await fs.promises.writeFile(dataFilePath,newData);
-    }catch(err){
-      console.log(err);
-      res.status(500).json({message:'Error occurred when deleting locator'});
-      return;
-    }
-    res.status(200).json({message:'Deleted locator'});
-}
+  const newData = JSON.stringify(locatorFile);
+  try {
+    await fs.promises.writeFile(dataFilePath, newData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Error occurred when deleting locator" });
+    return;
+  }
+  res.status(200).json({ message: "Deleted locator" });
+};
 
-exports.getPageNames=getPageNames;
-exports.getLocatorByPage=getLocatorByPage;
-exports.createLocatorByPage=createLocatorByPage;
-exports.deleteLocatorPage=deleteLocatorPage;
-exports.editLocatorPage=editLocatorPage;
+exports.getPageNames = getPageNames;
+exports.getLocatorByPage = getLocatorByPage;
+exports.createLocatorByPage = createLocatorByPage;
+exports.deleteLocatorPage = deleteLocatorPage;
+exports.editLocatorPage = editLocatorPage;
