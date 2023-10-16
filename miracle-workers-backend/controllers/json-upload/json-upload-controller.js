70630a2ebcd7b861b1adb85a7c99e89676dbfb81
Isuru <isurumadushangamage99@gmail.com>
2023-10-16 09:32:57 +0530

    final version 1

--- miracle-workers-backend/controllers/json-upload/json-upload-controller.js
+++ miracle-workers-backend/controllers/json-upload/json-upload-controller.js
@@ -9,10 +9,22 @@ const locatorFilePath = path.join(__dirname, '../../store/locator.json');
 const uploadJson = async (req, res, next) => {
   try {
     const { launcher, testSuite, data, locators } = req.body;
+    const dataSheet = data[0].map((dataSheet) => {
+      const dataSheetName = dataSheet[0];
+      const dataSheetType = dataSheetName[dataSheetName.length - 1];
+      if (dataSheetType === "E") {
+        const newDataSheet = dataSheet;
+        newDataSheet.splice(2, 0, [""]);
+        return newDataSheet;
+      } else {
+        return dataSheet
+      }
+    })
+    console.log('Devillllllllllllllllllllllllllllllll', dataSheet);
 
     await fs.promises.writeFile(launcherFilePath, JSON.stringify(launcher));
     await fs.promises.writeFile(testSuiteFilePath, JSON.stringify(testSuite));
-    await fs.promises.writeFile(dataFilePath, JSON.stringify(data[0]));
+    await fs.promises.writeFile(dataFilePath, JSON.stringify(dataSheet));
     await fs.promises.writeFile(locatorFilePath, JSON.stringify(locators[0]));
     return res.status(200).json({ message: 'File added' });
   } catch (error) {
